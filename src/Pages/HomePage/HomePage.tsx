a98c2216aa5d37a00d9f23742cccd0b44a18470f
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-12-03 16:17:44 +0300

    card component was created for the import methods of the variables.

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
-import "./homePage.css"
-type Props = {}
+import React from "react";
+import "./homePage.css";
+import { CategoryCard } from "../../components";
+import { categories } from "../../data/categoriesData";
+type Props = {};
 
 const HomePage = (props: Props) => {
   return (
-    <div className='home-layout'>HomePage</div>
-  )
-}
+    <div className="home-layout">
+      {categories.map((category, index) => (
+        <CategoryCard
+          key={index}
+          title={category.title}
+          description={category.description}
+          imageUrl={category.imageUrl}
+        />
+      ))}
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
---
+++ src/components/Cards/CategoryCards/CategoryCard.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type CategoryCardProps = {
+  title: string;
+  description: string;
+  imageUrl: string;
+};
+
+const CategoryCard: React.FC<CategoryCardProps> = ({title, description, imageUrl}) => {
+  return <div className="card__category">
+    <img src={imageUrl} alt={title} className="card__category__img"/>
+    <h3 className="card__category__title">{title}</h3>
+    <p className="card__category__description">{description}</p>
+  </div>;
+};
+
+export default CategoryCard;
