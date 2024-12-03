9924b67d0761c40ae4d207e99254a9e3b1744655
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-12-03 16:17:07 +0300

    A category data file was created for the import methods of the variables.

---
+++ src/data/categoriesData.ts
@@ -0,0 +1,12 @@
+export const categories = [
+    {
+      title: "Regression via Form",
+      description: "You can enter all data as manually.",
+      imageUrl: "https://via.placeholder.com/150",
+    },
+    {
+      title: "Regression via Upload",
+      description: "You can upload a xlsx file for import data.",
+      imageUrl: "https://via.placeholder.com/150",
+    },
+  ];
