9dc7d4af90852f99b6391f825e9c0f97bbdb5d5b
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-12-05 03:02:33 +0300

    modified sidebar & navbar

--- src/Pages/Layouts/Layout.tsx
+++ src/Pages/Layouts/Layout.tsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Router from "../../Routes/Router";
 import { Navbar, Sidebar } from "../../components";
 import Footer from "../../components/Footer/Footer";
-import "./layout.css"
+import "./layout.css";
 type Props = {};
-
+const sidebarLinks = [
+  { name: "What's regression and why is it used?", url: "/" },
+  { name: "What's the Linear Regression?", url: "/" },
+  { name: "What methods do we use when doing regression modeling?", url: "/" },
+];
 const Layout = (props: Props) => {
+    const [isCollapsed, setIsCollapsed] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsCollapsed((prev) => !prev);
+  };
   return (
-    <>
-      <Navbar />
-      <div className="d-grid grid-column-percent-15">
-        <Sidebar />
-        <div className="main-layout d-flex f-direction-column justify-flex-start padding-top-2">
+    <><Navbar />
+      <div
+        className={`d-grid ${
+          isCollapsed ? "grid-column-percent-3" : "grid-column-percent-15"
+        }`}
+      >
+        <Sidebar sidebarLinks={sidebarLinks} toggleSidebar={toggleSidebar}/>
+        <div className="main-layout d-flex f-direction-column justify-flex-start ">
+          
           <Router />
           <Footer />
         </div>
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,61 @@
-import React from 'react'
-import "./sidebar.css"
-type Props = {}
+import React, { useState } from "react";
+import "./sidebar.css";
+import { PiSticker } from "react-icons/pi";
+import { GiHamburgerMenu } from "react-icons/gi";
 
-const Sidebar = (props: Props) => {
+import { Link } from "react-router-dom";
+type sidebarLinks = {
+  name: string;
+  url: string;
+};
+
+type Props = {
+  sidebarLinks: sidebarLinks[];
+  toggleSidebar: () => void;
+};
+
+const Sidebar = ({ sidebarLinks, toggleSidebar }: Props) => {
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
+  const toggleSidebars = () => {
+    setIsCollapsed((prev) => !prev);
+  };
   return (
-    <div className='sidebar-layout'>Sidebar</div>
-  )
-}
+    <div className="sidebar-layout">
+      {}
+      <div
+        className={`${
+          isCollapsed
+            ? "button-container-collapsed"
+            : "button-container-NonCollapsed"
+        }`}
+        onClick={() =>{toggleSidebar() ; toggleSidebars()}} 
+      >
+        <GiHamburgerMenu />
 
-export default Sidebar
+      </div>
+      <div className="sidebar-layout__container d-flex f-direction-column gap-1 align-i-f-start">
+        {sidebarLinks.map((link, index) => (
+          <div key={index} className="d-flex align-i-center">
+            <span className="margin-top-4">
+            <PiSticker />
+            {" "}
+            </span>
+            <Link
+              to={link.url}
+              className={`sidebar-layout__container__link text small-text text-italic text-light-mode margin-top-4 ${
+                isCollapsed
+                  ? "transparent"
+                  : "opaque"
+              }`}
+            >
+              {link.name}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
